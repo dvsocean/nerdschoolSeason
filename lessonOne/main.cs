e3e4ae2d9395d29b88deb23d09d0c67a566b2a56
ircusa <dvsocean@protonmail.com>
2026-02-11 21:20:11 -0800

    adding lesson 2

--- lessonOne/main.cs
+++ lessonOne/main.cs
@@ -1,10 +1,51 @@
-Console.WriteLine("Hello Worlds");
+Console.WriteLine("----LESSON ONE-----");
 
-var strVar = "Hello";
+string strVar = "Hello";
+var StrVar = "Hello";
 var intVar = 98;
 var boolVar = false;
 
-Console.WriteLine($"iam string: {strVar}");
+Console.WriteLine(boolVar);
+
+Console.WriteLine("---- Variables & Primitive Types -------");
+
+int[] snacks = {3, 4, 55};
+string[] stringSnacks = {"stringOne", "stringTwo"};
+
+
+Console.WriteLine("List");
+ List<string> bag = new List<string>();
+ bag.Add("Item One");
+
+ Console.WriteLine("No interpolation");
+ Console.WriteLine(intVar);
+
+ Console.WriteLine($"With interpolation {intVar}");
+
+ Console.WriteLine("---- STORE APP ------");
+var taxRate = 10.5;
+var storeItemCost = 50;
+List<string> cart = new List<string>();
+cart.Add("ItemOne");
+cart.Add("ItemTwo");
+cart.Add("ItemThree");
+
+var storeName = "NerdSchoolApp";
+var sum = cart.Count * storeItemCost;
+
+var totalTax = sum * taxRate;
+var grandTotal = sum + totalTax;
+
+Console.WriteLine($"Welcome to {storeName}. You have {cart.Count} items in your bag");
+Console.WriteLine($"Your total is: {grandTotal}");
+
+
+
+
+ 
+ 
+ 
+
 
 
 
